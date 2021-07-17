d53536a808ff9ed3bf9a69a0707c211f45235d54
Jason Smith <jason@elementalreasoning.com>
2021-07-16 17:27:27 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='erlogging',
+      version='0.9',
+      description='Logging utility',
+      url='http://github.com/jasonmccsmith/erlogging',
+      author='Elemental Reasoning',
+      author_email='jason@elementalreasoning.com',
+      license='BSD-3',
+      packages=['erlogging'],
+      zip_safe=False)
